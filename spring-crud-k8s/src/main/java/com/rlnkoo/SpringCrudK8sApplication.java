a9e71bdaa1d3c4c220100aa29ea186a77d64d3cc
Artur Rolak <rolakartur96@gmail.com>
2024-11-18 15:32:58 +0100

    CRUD App finished

---
+++ spring-crud-k8s/src/main/java/com/rlnkoo/SpringCrudK8sApplication.java
@@ -0,0 +1,13 @@
+package com.rlnkoo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCrudK8sApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCrudK8sApplication.class, args);
+	}
+
+}
